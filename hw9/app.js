a9f139e2f40acd537a3b16d5953e7409f246571d
DFeduk <76042333+DFeduk@users.noreply.github.com>
2021-05-31 13:41:08 +0300

    Add files via upload

--- hw9/app.js
+++ hw9/app.js
@@ -1,5 +1,16 @@
-let usersName;
-do {
-  usersName = prompt("What`s your name?");
-} while (usersName === "" || usersName === null);
-document.getElementById("heading").innerText = `Hello, ${usersName}!`;
+function getUserName(string) {
+  let userName;
+  do {
+    userName = prompt(string);
+  } while (isNameValid(userName));
+  return userName;
+}
+
+function isNameValid(userName) {
+  return userName === "" || userName === null;
+}
+function getUserGreeting(userName) {
+  return (document.getElementById("heading").innerText = `Hello, ${userName}!`);
+}
+const userName = getUserName("What`s your name?");
+const greeting = getUserGreeting(userName);
