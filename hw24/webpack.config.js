de28d96177347b45df4edbb4af98f926852a7de6
Danylo <mr.fedyk@gmail.com>
2021-08-08 19:29:47 +0300

    no message

--- hw24/webpack.config.js
+++ hw24/webpack.config.js
@@ -5,7 +5,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = [
   {
     entry: "./src/js/app.js",
-    mode: "development",
     output: {
       filename: "bundle.js",
       path: path.resolve(__dirname, "dist"),
